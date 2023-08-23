2556f8bfdc236130c5073e614b498c486b09d824
Reza Soltani <reza.soltani@gmail.com>
2023-08-23 12:36:59 +0330

    add RouterProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
+import { RouterProvider } from "react-router-dom"
+import Login from "./features/identity/components/login"
+import Register from "./features/identity/components/register"
+import router from "./router"
 
 
 function App() {
 
   return (
-    <p>پلتفرم آموزش آنلاین</p>
+    <RouterProvider router={router} />
   )
 }
 
