812a37eeff9affe57c1143d26c44b2f6ca2ef5b6
Reza Soltani <reza.soltani@gmail.com>
2023-09-11 20:06:42 +0330

    add interceptor

--- src/core/http-service.js
+++ src/core/http-service.js
@@ -6,3 +6,30 @@ export const httpService = axios.create({
     baseURL: BASE_URL,
 })
 
+export const httpInterceptedService = axios.create({
+    baseURL: BASE_URL
+})
+
+httpInterceptedService.interceptors.request.use(
+    async (config) => {
+        const token = localStorage.getItem('token')
+
+        if(token) {
+            config.headers = {
+                authorization: `Bearer ${token}`
+            }
+        }
+        return config
+    },
+    (error) => Promise.reject(error)
+)
+
+httpInterceptedService.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        if (error.response.status === 401) {
+            window.location.href = '/login'
+        }
+        return Promise.reject(error)
+    }
+)
